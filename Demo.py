06928ce9da4b25cf2c7a596c289b2460a57f8d90
Rashid <34310411+frankenstein32@users.noreply.github.com>
2018-10-02 00:44:24 +0530

    Add files via upload

---
+++ Demo.py
@@ -0,0 +1,19 @@
+import heapq
+
+h = [2, 3, 1, 19, 29, 3, 1]
+heapq.heappush(h,2)
+heapq.heappush(h,3)
+heapq.heappush(h,1)
+heapq.heappush(h,10)
+heapq.heappush(h,19)
+heapq.heappush(h,29)
+heapq.heappush(h,3)
+heapq.heappush(h,1)
+
+print(h)
+# print(heapq.heappop(h))
+# print(heapq.heappop(h))
+# print(heapq.heappop(h))
+# print(heapq.heappop(h))
+# print(heapq.heappop(h))
+# print(heapq.__about__)
